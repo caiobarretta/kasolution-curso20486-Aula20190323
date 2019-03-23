d2fc595418e82fd9328f446787113cb1a14ccf34
Caio Augusto Barretta <caio.barretta@gmail.com>
2019-03-23 16:56:44 -0300

    Commit MOD4
    Commit MOD4

---
+++ MOD4/FN.Store/FN.Store.UI/Controllers/ProdutosController.cs
@@ -0,0 +1,25 @@
+﻿using FN.Store.UI.Data;
+using FN.Store.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FN.Store.UI.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private readonly StoreDataContext _ctx;
+
+        public ProdutosController(IConfiguration config) => _ctx = new StoreDataContext(config);
+
+        public ViewResult Index()
+        {
+            var data = _ctx.Produtos.ToList();
+            return View(data);
+        }
+    }
+}
